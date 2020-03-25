46a3ee92df46d665c07f2659e678f1895c0fad27
alapatbh <44811890+alapatbh@users.noreply.github.com>
2020-03-24 21:26:32 -0400

    initial commit

---
+++ src/main/java/com/ls/microservices/laststopfrontend/LastStopFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.ls.microservices.laststopfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastStopFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastStopFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ls/microservices/laststopfrontend/LastStopFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.microservices.laststopfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastStopFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
